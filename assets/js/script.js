d1b330b926d46c758ef8a0d44c671bba8a20292a
Develogenics <>
2023-07-16 17:09:15 +0000

    background image and parralax completed in desk view

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .to(".rock", 10, { y: -10 })
+  .to(".rock", 10, { y: "-=50" })
   .to(".girl", 10, { x: "+=610", y: "-=100" }, "-=10")
-  .to(".man", 10, { x: "-=610", y: "+=130" }, "-=10")
+  .to(".man", 10, { x: "-=610", y: "+=330" }, "-=10")
   .to(".tree", 10, { y: -400 }, "-=10")
   .fromTo(".bg1", { y: -40 }, { y: 0, duration: 10 }, "-=10")
   .to(".content", 10, { bottom: "0%" }, "-=10")
